d55ba34edec9abfe723cd3e343571a506c72a3c8
holgrave777 <122122682+holgrave777@users.noreply.github.com>
2024-06-30 15:34:40 -0700

    Update uv.js

--- public/uv.js
+++ public/uv.js
@@ -1,6 +1,6 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('/public/uv/uv.bundle.js');
+importScripts('/public/uv/uv.config.js');
+importScripts('/public/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
